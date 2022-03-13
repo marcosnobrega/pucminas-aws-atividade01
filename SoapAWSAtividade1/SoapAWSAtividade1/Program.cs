3eb4918e2cd5c1bca52c409c3a98dcaf466bd2c5
Marcos Costa <marcos.costa@virtus.ufcg.edu.br>
2022-03-13 17:11:42 -0300

    Initial commit

---
+++ SoapAWSAtividade1/SoapAWSAtividade1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SoapAWSAtividade1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ServiceReference1.SOAPDemoSoapClient client = new ServiceReference1.SOAPDemoSoapClient();
+
+            var response = await client.LookupCityAsync("84119");
+
+            Console.WriteLine($"{response.City}");
+
+            var intResponse = await client.AddIntegerAsync(25, 20);
+
+            Console.WriteLine($"{intResponse.ToString()}");
+
+            var personResponse = await client.FindPersonAsync("1");
+
+            Console.WriteLine($"{personResponse.Name}");
+        }
+    }
+}
